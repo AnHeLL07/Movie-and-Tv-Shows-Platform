454ca6f5519d90cf08e331b6bf0b14cf67935d9c
Andrei Anghel <andreianghel97@ymail.com>
2022-08-12 01:29:59 +0300

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'b6b82ad5cd61ae318ef7ca2dc1159a40',
+    apiKey: 'your api key',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
